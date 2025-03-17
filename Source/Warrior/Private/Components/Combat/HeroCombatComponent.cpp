a7899e0c52d970680abc384decaf19167c284dfb
MustafaKumHandler <kum.mustafa96@gmail.com>
2025-03-17 22:44:40 +0300

    HitPause / HitReaction
    HitPause / HitReaction

--- Source/Warrior/Private/Components/Combat/HeroCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/HeroCombatComponent.cpp
@@ -41,9 +41,19 @@ void UHeroCombatComponent::OnHitTargetActor(AActor* HitActor)
 		WarriorGameplayTags::Shared_Event_MeleeHit,
 		Data
 	);
+
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+		GetOwningPawn(),
+		WarriorGameplayTags::Player_Event_HitPause,
+		FGameplayEventData()
+	);
 }
 
 void UHeroCombatComponent::OnWeaponPulletFromTargetActor(AActor* InteractedActor)
 {
-	
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+		GetOwningPawn(),
+		WarriorGameplayTags::Player_Event_HitPause,
+		FGameplayEventData()
+	);
 }
