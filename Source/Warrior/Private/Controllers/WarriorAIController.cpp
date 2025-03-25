cf69ce06d3b4f1d82b15c0b81839bcbb9ec1620f
MustafaKumHandler <kum.mustafa96@gmail.com>
2025-03-25 20:17:26 +0300

    Added AIController
    Added AIController

---
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -0,0 +1,16 @@
+// KumDev All Rights Reserved
+
+
+#include "Controllers/WarriorAIController.h"
+#include "Navigation/CrowdFollowingComponent.h"
+
+#include "WarriorDebugHelper.h"
+
+AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
+{
+	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		Debug::Print(TEXT("CrowdFollowingComponent is valid"), FColor::Green);
+	}
+}
