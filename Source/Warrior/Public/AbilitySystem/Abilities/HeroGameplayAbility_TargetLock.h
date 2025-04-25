63181ad998ccdb1a5d2ea7539e39dafa5f56f5c5
MustafaKumHandler <kum.mustafa96@gmail.com>
2025-04-25 19:17:31 +0300

    Target Lock System Continue
    Target Lock System Continue

--- Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
+++ Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
@@ -20,4 +20,31 @@ protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	// ~ End UGameplayAbility Interface
+
+private:
+	void TryLockOnTarget();
+	void GetAvailableActorsToLock();
+
+	AActor* GetNearestTargetFromAvailableActors(const TArray<AActor*>& InAvailableActors);
+
+	void CancelTargetLockAbility();
+	void CleanUp();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	float BoxTraceDistance = 5000.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	FVector TraceBoxSize = FVector(5000.0f, 5000.0f, 300.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	TArray< TEnumAsByte < EObjectTypeQuery > > BoxTraceChannel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	bool bShowPersistentDebugShape = false;
+
+	UPROPERTY()
+	TArray<AActor*> AvailableActorsToLock;
+
+	UPROPERTY()
+	AActor* CurrentLockedActor;
 };
