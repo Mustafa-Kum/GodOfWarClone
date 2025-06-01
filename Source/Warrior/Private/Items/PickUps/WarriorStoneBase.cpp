be60feb297154b97490abede39ab8cc6f3d4b945
MustafaKumHandler <kum.mustafa96@gmail.com>
2025-06-01 15:49:58 +0300

    HeavyAbility Montage / HeavyAbility AOE Damage / Stone Class / BP Stone / Pick Up Ability / On Stone Begin Overlap
    HeavyAbility Montage / HeavyAbility AOE Damage / Stone Class / BP Stone / Pick Up Ability / On Stone Begin Overlap

---
+++ Source/Warrior/Private/Items/PickUps/WarriorStoneBase.cpp
@@ -0,0 +1,15 @@
+// KumDev All Rights Reserved
+
+
+#include "Items/PickUps/WarriorStoneBase.h"
+#include "Characters/WarriorHeroCharacter.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "WarriorGameplayTags.h"
+
+void AWarriorStoneBase::OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (AWarriorHeroCharacter* OverlappedHeroCharacter = Cast<AWarriorHeroCharacter>(OtherActor))
+	{
+		OverlappedHeroCharacter->GetWarriorAbilitySystemComponent()->TryActivateAbilityByTag(WarriorGameplayTags::Player_Ability_PickUp_Stones);
+	}
+}
